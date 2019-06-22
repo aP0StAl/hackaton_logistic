2a2c52f6719b3e13cd752ce0986b26d610a6424e
S.Poryadnyi <S.Poryadnyi@abcconsulting.ru>
2019-06-22 21:08:37 +0300

    init cars, routes

--- src/main/java/ru/hackaton/logistic/crutch/DBInit.java
+++ src/main/java/ru/hackaton/logistic/crutch/DBInit.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.hackaton.logistic.domain.Order;
+import ru.hackaton.logistic.domain.Route;
 import ru.hackaton.logistic.repository.OrderRepository;
+import ru.hackaton.logistic.repository.RouteRepository;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
@@ -14,10 +16,12 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class DBInit {
     private final OrderRepository orderRepository;
+    private final RouteRepository routeRepository;
 
     @PostConstruct
     public void initAll(){
         initOrders();
+        initRoutes();
     }
 
     private void initOrders(){
@@ -28,6 +32,22 @@ public class DBInit {
             Order[] orders = mapper.readValue(file, Order[].class);
             for (Order order : orders) {
                 orderRepository.save(order);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void initRoutes(){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            File file = new File(classLoader.getResource("jsons/routes.json").getFile());
+            Route[] routes = mapper.readValue(file, Route[].class);
+            for (Route route : routes) {
+                routeRepository.save(route);
             }
         } catch (IOException e) {
             e.printStackTrace();
