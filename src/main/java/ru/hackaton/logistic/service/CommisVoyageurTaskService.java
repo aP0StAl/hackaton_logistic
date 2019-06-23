d66872f74a8b8a67b6bbfca2cd6fb589fe53fa47
vladnosiv <vladnosiv@gmail.com>
2019-06-23 13:28:09 +0300

    Исправлен баг в построении маршрута

--- src/main/java/ru/hackaton/logistic/service/CommisVoyageurTaskService.java
+++ src/main/java/ru/hackaton/logistic/service/CommisVoyageurTaskService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import ru.hackaton.logistic.domain.GeoPoint;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -65,6 +66,8 @@ public class CommisVoyageurTaskService {
             cur = path[mask][cur];
             mask = mask & (~(1 << last));
         }
+
+        Collections.reverse(answerPath);
 
         assert(answerPath.size() != n);
 
