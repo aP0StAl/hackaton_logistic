afff0175005be52c43eee668d6c15bb9ea48ccf7
S.Poryadnyi <S.Poryadnyi@abcconsulting.ru>
2019-06-23 11:05:26 +0300

    distanceMap

--- src/main/java/ru/hackaton/logistic/service/DistanceService.java
+++ src/main/java/ru/hackaton/logistic/service/DistanceService.java
@@ -3,6 +3,10 @@ package ru.hackaton.logistic.service;
 import org.springframework.stereotype.Service;
 import ru.hackaton.logistic.domain.GeoPoint;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class DistanceService {
     public double getDistance(GeoPoint a, GeoPoint b){
@@ -13,5 +17,17 @@ public class DistanceService {
         double ac = Math.acos(Math.sin(a.getLat()) * Math.sin(b.getLat()) +
                 Math.cos(a.getLat()) * Math.cos(b.getLat()) * Math.cos(a.getLon() - b.getLon()));
         return ac * 6371;
+    }
+
+    private Map<GeoPoint, Map<GeoPoint, Double>> getDistanceMap(List<GeoPoint> points){
+        Map<GeoPoint, Map<GeoPoint, Double>> distanceMap = new HashMap<>();
+        for (GeoPoint p1 : points) {
+            Map<GeoPoint, Double> subMap = new HashMap<>();
+            for (GeoPoint p2: points){
+                subMap.put(p2, getDistance(p1, p2));
+            }
+            distanceMap.put(p1, subMap);
+        }
+        return distanceMap;
     }
 }
